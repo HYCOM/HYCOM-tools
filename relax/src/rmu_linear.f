d731a43a8074533bd7f23c2bfd890a3a30bdbf93
Alan J. Wallcraft <awallcraft@coaps.fsu.edu>
2019-05-23 17:33:46 +0000

    initialize rvmin in rmu_linear.f

--- relax/src/rmu_linear.f
+++ relax/src/rmu_linear.f
@@ -65,6 +65,8 @@ C
       CHARACTER*1 C(-1:9)
       DATA C / '*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' /
 C
+      RVMIN = HUGE(RVMIN)
+C
       CALL XCSPMD
       ALLOCATE(   RMU(IDM,JDM) )
       ALLOCATE(  IRMU(IDM,JDM) )
